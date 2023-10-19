15de739ae5b4b99affcc1ba5a3c5f9241ddd3d2f
kevin <9047112+kevL974@users.noreply.github.com>
2023-10-19 12:10:13 +0200

    collect_stream_data() function can download fresh data from binance. But data needs to be processed to be saved

--- opa/harvest/collect.py
+++ opa/harvest/collect.py
@@ -2,12 +2,59 @@ import argparse
 import asyncio
 import os
 from datetime import datetime
-from binance import AsyncClient
+from binance import AsyncClient, BinanceSocketManager
 from opa.storage.connector import InputOutputStream, CsvConnector
 from typing import List
 
 API_KEY = os.getenv("API_KEY_BINANCE_TESTNET")
 API_SECRET = os.getenv("API_KEY_SECRET_BINANCE_TESTNET")
+
+
+async def get_missing_data(client: AsyncClient, symbol: str, interval: str, output: InputOutputStream) -> None:
+    """
+    Get earliest data from websocket stream.
+    :param client: Binance AsyncClient from Binance API
+    :param symbol: Targeted symbol.
+    :param interval: Targeted interval.
+    :param output: Place where data will be saved.
+    :return:
+    """
+    klines = await client.get_historical_klines(symbol, interval, "1 years ago UTC")
+    for kline in klines:
+        output.write(kline, None)
+
+
+async def start_collecting(client: AsyncClient, symbol: str, interval: str, output: InputOutputStream) -> None:
+    """
+    Get latest data from websocket stream.
+    :param client:
+    :param symbol:
+    :param interval:
+    :param output:
+    :return:
+    """
+
+    bm = BinanceSocketManager(client)
+    ks = bm.kline_socket(symbol, interval)
+    async with ks as kscm:
+        while True:
+            res = await kscm.recv()
+            output.write(res, None)
+
+
+async def start_stream_data_collector(client: AsyncClient, symbol: str, interval: str, output: InputOutputStream) -> None:
+    """
+    Updates missing data from historical data and collect data from Binance web socket for symbol and interval given
+    in parameter and saves them in output.
+    :param client: Binance AsyncClient from Binance API
+    :param symbol: Targeted symbol.
+    :param interval: Targeted interval.
+    :param output: Place where data will be saved.
+    :return:
+    """
+    await get_missing_data(client, symbol, interval, output)
+    await start_collecting(client, symbol, interval, output)
+
 
 def collect_hist_data(symbols: List[str], intervals: List[str], output: InputOutputStream) -> None:
     """
@@ -22,19 +69,22 @@ def collect_hist_data(symbols: List[str], intervals: List[str], output: InputOut
     pass
 
 
-def collect_stream_data(symbols: List[str], intervals: List[str], output: InputOutputStream) -> None:
+async def collect_stream_data(symbols: List[str], intervals: List[str], output: InputOutputStream) -> None:
     """
-    Collect data in real time foreach symbols and intervals and save them in output given in parameter.
+    Collects data in real time foreach symbols and intervals and save them in output given in parameter.
     :param symbols: List of targeted symbols e.g ["BTCUSDT", "ETHBTC"]
     :param intervals: List of candleline intervals in string format e.g ["1m", "15m"]
     :return:
     """
-    # for symbol_i in symbols:
-    #     for interval_j in intervals:
+    client = await AsyncClient.create(api_key=API_KEY, api_secret=API_SECRET, testnet=True)
+    collectors = []
+    for symbol_i in symbols:
+        for interval_j in intervals:
+            collectors.append(asyncio.ensure_future(start_stream_data_collector(client, symbol_i, interval_j, output)))
 
-    # écrire les données récupérées avec cette méthode
-    # output.write()
-    pass
+    finished, _ = await asyncio.wait(collectors)
+    client.close_connection()
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Make some trades with Binance')
