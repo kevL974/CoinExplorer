cd6ddad03f7c5e174eee92ccffaa789b4fa03f10
BALDEGN <79610944+BALDEGN@users.noreply.github.com>
2023-11-06 14:21:30 +0100

    création d'une classe Candlestick

--- opa/core/Candlestick.py
+++ opa/core/Candlestick.py
@@ -15,14 +15,3 @@ class Candlestick:
         return date
 
 
-# Exemple
-candlestick = Candlestick("BTCUSDT", "15m", 4000.0, 4000.5, 4001.0, 3999.5, 10000.0, 1640805600)
-print("Devise:", candlestick.devise)
-print("Intervalle:", candlestick.intervalle)
-print("Open:", candlestick.open)
-print("Close:", candlestick.close)
-print("High:", candlestick.high)
-print("Low:", candlestick.low)
-print("Volume:", candlestick.volume)
-print("Close Time (Timestamp):", candlestick.close_time)
-print(candlestick.date())
