fbc6ed9707c19b5ee30706fe82a70ca91ad9492c
kevin <9047112+kevL974@users.noreply.github.com>
2023-11-06 19:31:00 +0100

    renaming Candlestick.py to candlestick.py

--- opa/core/candlestick-runner.py
+++ opa/core/candlestick-runner.py
@@ -1,5 +1,5 @@
 # Exemple d'utilisation
-from Candlestick import Candlestick
+from candlestick import Candlestick
 candlestick = Candlestick("BTCUSDT", "15m", 4000.0, 4000.5, 4001.0, 3999.5, 10000.0, 1640805600)
 print("Devise:", candlestick.devise)
 print("Intervalle:", candlestick.intervalle)
