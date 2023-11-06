89b319de3f8a64033e2eb9f91da270c593c28b49
BALDEGN <79610944+BALDEGN@users.noreply.github.com>
2023-11-06 19:14:13 +0100

    création d'une classe Candlestick

--- opa/core/Candlestick.py
+++ opa/core/Candlestick.py
@@ -1,17 +1,16 @@
 class Candlestick:
-    def __init__(self, devise, intervalle, open_price, close_price, high, low, volume, close_time):
-        self.devise = str(devise)
-        self.intervalle = str(intervalle)
-        self.open = float(open_price)
-        self.close = float(close_price)
-        self.high = float(high)
-        self.low = float(low)
-        self.volume = float(volume)
-        self.close_time = int(close_time)
+    def __init__(self, devise: str, intervalle: str, open_price: float, close_price: float, high: float, low: float,
+                 volume: float, close_time: int):
+        self.devise = devise
+        self.intervalle = intervalle
+        self.open = open_price
+        self.close = close_price
+        self.high = high
+        self.low = low
+        self.volume = volume
+        self.close_time = close_time
 
     def date(self):
         import datetime
         date = datetime.datetime.fromtimestamp(self.close_time).strftime('%Y-%m-%d')
         return date
-
-
