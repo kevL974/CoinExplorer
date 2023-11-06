f415f009038156291b3627246b5e463d14290560
kevin <9047112+kevL974@users.noreply.github.com>
2023-11-06 18:34:13 +0100

    reformating  connector.py

--- opa/storage/connector.py
+++ opa/storage/connector.py
@@ -24,8 +24,14 @@ class CsvConnector(InputOutputStream):
 
 class KafkaConnector(InputOutputStream):
 
-    def __init__(self, bootstrapservers: str = "localhost:9092", clientid: str = "opa_collector", valueserializer=lambda v: v.encode("utf-8")):
-        self.kafka_producer = KafkaProducer(bootstrap_servers=bootstrapservers, client_id=clientid, value_serializer=valueserializer,  api_version=(2, 8, 1))
+    def __init__(self,
+                 bootstrapservers: str = "localhost:9092",
+                 clientid: str = "opa_collector",
+                 valueserializer=lambda v: v.encode("utf-8")):
+        self.kafka_producer = KafkaProducer(bootstrap_servers=bootstrapservers,
+                                            client_id=clientid,
+                                            value_serializer=valueserializer,
+                                            api_version=(2, 8, 1))
 
     def write(self, data: Dict, **options) -> None:
         self.kafka_producer.send(value=data.__str__(), **options)
