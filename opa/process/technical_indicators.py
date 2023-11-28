b1d4ff438d95bb8e0e0ce9c425d07d05fe17fbf6
kevin <9047112+kevL974@users.noreply.github.com>
2023-11-28 12:18:36 +0100

    removing useless  comments

--- opa/process/technical_indicators.py
+++ opa/process/technical_indicators.py
@@ -1,22 +1,17 @@
 from pyspark.sql import functions as F
 from pyspark.sql.window import Window
+from pyspark.sql import SparkSession
+
 
 def calculate_moving_average(df, n: int):
-    # Je vérifie si la colonne "CANDLESTICK:close" existe dans mon DataFrame
     if "CANDLESTICK:close" not in df.columns:
         raise ValueError("La colonne 'CANDLESTICK:close' n'existe pas dans mon DataFrame.")
-    
-    # Je crée une fenêtre pour le calcul de la moyenne mobile
+
     window_spec = Window.orderBy("timestamp").rowsBetween(-n+1, Window.currentRow)
-    
-    # J'ajoute une colonne avec la moyenne mobile calculée sur n périodes
+
     df_with_ma = df.withColumn(f"MA-{n}", F.avg("CANDLESTICK:close").over(window_spec))
     
     return df_with_ma
-
-# exemple d'utilisation
-
-from pyspark.sql import SparkSession
 
 spark = SparkSession.builder \
     .appName("TestMovingAverage") \
@@ -24,17 +19,13 @@ spark = SparkSession.builder \
 data = [("2023-01-01", 10),
         ("2023-01-02", 15),
         ("2023-01-03", 20),
-        
        ]
 
 columns = ["timestamp", "CANDLESTICK:close"]
 
 df = spark.createDataFrame(data, columns)
 
-
-# output: print(df)  DataFrame[timestamp: string, CANDLESTICK:close: bigint]
-
-n = 3  # pour trois périodes
+n = 3
 df_with_ma = calculate_moving_average(df, n)
 
 df_with_ma.show()
