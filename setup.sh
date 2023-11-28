8dde6b444626d4d0c4cae30c5ba7741f8f761517
kevin <9047112+kevL974@users.noreply.github.com>
2023-11-28 15:47:03 +0100

    source image to alpine instead of bullseye for images : opa_collect, opa_hbase, opa_streaming +fixing issue with setup.sh

--- setup.sh
+++ setup.sh
@@ -4,10 +4,14 @@ docker image build \
 --tag opa_collect:latest \
 --build-arg api_key=${API_KEY_BINANCE_TESTNET} \
 --build-arg api_secret=${API_KEY_SECRET_BINANCE_TESTNET} \
---file docker/collect/Dockerfile .
+--file docker/collect/Dockerfile . && \
+
+docker image build \
+--tag opa_streaming:latest \
+--file docker/streaming/Dockerfile . && \
 
 docker image build \
 --tag opa_hbase:latest \
---file docker/hbase/Dockerfile .
+--file docker/hbase/Dockerfile . && \
 
 docker volume prune -f && docker-compose -f docker/docker-compose.yml up --scale kafka=3 -d
