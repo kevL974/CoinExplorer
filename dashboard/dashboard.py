70e707333564d8e90580f046eece786083ace53e
arnau <arnaud.rioux@neuf.fr>
2023-12-06 15:39:06 +0100

    update Dashboard

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -7,47 +7,22 @@ import pandas as pd
 import os
 
 OPA_API_URL: str = os.getenv("OPA_API_URL")
-
+color = '#161d22'
+color_text = 'mediumturquoise'
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 app.layout = dbc.Container(
     [
-        dcc.Markdown('## **CryptoBot avec Binance** ',
-                     style={'textAlign': 'center',
-                            'justification': 'center',
-                            'color': 'mediumturquoise',
-                            'height': 60,
-                            'background': '#4B5F63'}),
-        dbc.Row(
-            [
-                dbc.Col(
-                    [
-                        dcc.Markdown('Choix de la date ',
-                                     style={'textAlign': 'center',
-                                            'color': 'mediumturquoise',
-                                            'width': '100%',
-                                            'background': '#4B5F63'}),
-                    ],
-                ),
-                dbc.Col(
-                    [
-                        dcc.Markdown('Choix de la monnaie ',
-                                     style={'textAlign': 'center',
-                                            'color': 'mediumturquoise',
-                                            'width': '100%',
-                                            'background': '#4B5F63'}),
-                    ]
-                ),
-                dbc.Col(
-                    [
-                        dcc.Markdown('Choix de la périodicité',
-                                     style={'textAlign': 'center',
-                                            'color': 'mediumturquoise',
-                                            'width': '100%',
-                                            'background': '#4B5F63'}),
-                    ]
-                ),
-
-            ]),
+        dbc.Row([
+            dcc.Markdown('## **CryptoBot avec Binance** ',
+                         style={"position": "fixed",
+                                "top": 0,
+                                "left": 0,
+                                "bottom": 0,
+                                "padding": "1rem",
+                                'textAlign': 'center',
+                                'color': color_text,
+                                'background': color}),
+        ]),
 
         dbc.Row(
             [
@@ -55,60 +30,122 @@ app.layout = dbc.Container(
                     [
                         dbc.Row(
                             [
+                                dcc.Markdown('Choix de la date ',
+                                             style={'textAlign': 'center',
+                                                    'color': 'mediumturquoise',
+                                                    'width': '100%',
+                                                    'background': color}),
                                 dcc.DatePickerRange(
                                     id='interval_date',
+                                    clearable=True,
+                                    style={'textAlign': 'center',
+                                           'color': 'mediumturquoise',
+                                           'background': color},
                                     min_date_allowed=date(2017, 8, 1),
-                                    initial_visible_month=date(2023, 12, 1),
                                     start_date=date(2023, 9, 1),
-                                    end_date=datetime.now().date(),
+                                    end_date=date(2023, 12, 1),
+                                    with_portal=True,
                                     display_format='DD MMM YY',
                                 ),
-                                html.Div(id='output-container-date-picker-range'),
+                                html.Div(id='output-container-date-picker-range',
+                                         style={'textAlign': 'center',
+                                                'color': 'mediumturquoise',
+                                                'background': color}),
                             ],
                         ),
                     ], className="d-flex justify-content-center align-items-stretch"),
                 dbc.Col(
                     [
-                        dcc.RadioItems(['BTCUSDT', 'ETHUSDT'], 'BTCUSDT',
-                                       id='symbol_radio', inline=False),
+                        dbc.Row(
+                            [
+                                dcc.Markdown('Choix de la monnaie ',
+                                             style={'textAlign': 'center',
+                                                    'color': 'mediumturquoise',
+                                                    'width': '100%',
+                                                    'background': color}),
+                                dcc.RadioItems(['BTCUSDT', 'ETHUSDT'],
+                                               'BTCUSDT',
+                                               id='symbol_radio',
+                                               inline=False,
+                                               style={'textAlign': 'center',
+                                                      'color': 'mediumturquoise',
+                                                      'width': '100%',
+                                                      'background': color}),
+                            ],
+                        ),
                     ], className="d-flex justify-content-center align-items-stretch"
                 ),
                 dbc.Col(
                     [
-                        dcc.RadioItems(options={"1m": '1 minute',
-                                                "3m": '3 minutes',
-                                                "5m": '5 minutes',
-                                                "15m": '15 minutes',
-                                                "30m": '30 minutes'},
-                                       id='interval_radio',
-                                       value='15m',
-                                       inline=False),
+                        dbc.Row(
+                            [
+                                dcc.Markdown('Choix de la périodicité',
+                                             style={'textAlign': 'center',
+                                                    'color': 'mediumturquoise',
+                                                    'width': '100%',
+                                                    'background': color}),
+                                dcc.RadioItems(options={"1m": '1 minute',
+                                                        "3m": '3 minutes',
+                                                        "5m": '5 minutes',
+                                                        "15m": '15 minutes',
+                                                        "30m": '30 minutes'},
+                                               id='interval_radio',
+                                               value='15m',
+                                               inline=False,
+                                               style={'textAlign': 'center',
+                                                      'color': 'mediumturquoise',
+                                                      'width': '100%',
+                                                      'background': color}),
+                            ],
+                        ),
                     ], className="d-flex justify-content-center align-items-stretch"
                 ),
 
-            ]),
+            ], style={"position": "fixed",
+                      "top": 100,
+                      "left": 0,
+                      "width": "100%",
+                      'textAlign': 'center',
+                      'justification': 'center',
+                      'color': 'mediumturquoise',
+                      'background': color}),
+
         html.Div([
-            dbc.Button("Lancer la requete", "button",
-                       active='True',
-                       value=['True']),
-        ], className="d-flex justify-content-center "),
+            dbc.Button("Lancer la requete", "button"),
+        ],
+            style={"position": "fixed",
+                   "top": 300,
+                   "left": 0,
+                   "width": "100%",
+                   "padding": "1rem",
+                   'textAlign': 'center',
+                   'justification': 'center'},
+            className="d-flex justify-content-center "),
 
         dbc.Row(
             [
                 dcc.Graph(id='graph',
-                          style={'textAlign': 'center',
-                                 'color': 'mediumturquoise',
-                                 'width': '100%',
-                                 'background': '#4B5F63'}),
-            ], className="d-flex justify-content-center bg-#4B5F63 "),
+                          figure={'layout': {'plot_bgcolor': color,
+                                             'paper_bgcolor': color, }}
+                          ),
+            ], style={"position": "fixed",
+                      "top": 380,
+                      "left": 0,
+                      "high": "100%",
+                      "width": "100%",
+                      "padding": "1rem",
+                      'textAlign': 'center',
+                      'justification': 'center',
+                      'color': 'mediumturquoise',
+                      'background': color},
+            className="d-flex justify-content-center bg-#4B5F63 "),
         dcc.Checklist(
             id="toggle-rangeslider",
-            options=[{"label": "Include Rangeslider", "value": "slider"}],
+            options=[{"label": "Include Rangeslider",
+                      "value": "slider"}],
             value=["slider"],
         ),
-
-    ])
-
+    ], )
 
 @app.callback(
     Output("graph", "figure"),
@@ -134,7 +171,8 @@ def display_candlestick(value, symbol: str, interval: str, start_date: str, end_
                 high=df['CANDLESTICKES:high'],
                 low=df['CANDLESTICKES:low'],
                 close=df['CANDLESTICKES:close'],
-                increasing_line_color='#6DE47A', decreasing_line_color='#FF4D4D',
+                increasing_line_color='#6DE47A',
+                decreasing_line_color='#FF4D4D',
 
             ))
 
@@ -148,10 +186,23 @@ def display_candlestick(value, symbol: str, interval: str, start_date: str, end_
         print(f"erreur {response.status_code}")
 
     fig.update_layout(
-        title='Evolution de la monnaie',
+        title_text='Evolution de la monnaie', title_x=0.5,
         yaxis_title='Value',
-        height=800)
-
+        plot_bgcolor=color, autosize=True, height=800, font_color='mediumturquoise', paper_bgcolor=color)
+    fig.update_xaxes(
+        mirror=True,
+        ticks='outside',
+        showline=True,
+        linecolor='mediumturquoise',
+        gridcolor='mediumturquoise', color="mediumturquoise"
+    )
+    fig.update_yaxes(
+        mirror=True,
+        ticks='outside',
+        showline=True,
+        linecolor='mediumturquoise',
+        gridcolor='mediumturquoise', color="mediumturquoise"
+    )
     fig.update_traces(visible=True, )
     return fig
 
