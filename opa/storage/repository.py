df3933a746e81a986d2c2938d74fe21b26ebff51
kevin <toto@gmail.com>
2024-03-04 11:34:58 +0100

    implemention of methods finished

--- opa/storage/repository.py
+++ opa/storage/repository.py
@@ -1,7 +1,6 @@
-from typing import List, Optional
+from typing import List, Dict, Optional
 from opa.storage.abstract_model import HbaseEntity
 from opa.utils import retry_connection_on_brokenpipe, retry_connection_on_ttransportexception
-import pandas as pd
 import happybase as hb
 
 
@@ -10,7 +9,7 @@ class HbaseCrudRepository:
     Class for generic CRUD operations on a Hbase repository for a specific type.
     """
 
-    def __init__(self, table_name: str, schema: Dict[str,Dict], host="localhost", port=9090, pool_size=3):
+    def __init__(self, table_name: str, schema: Dict[str, Dict], host="localhost", port=9090, pool_size=3):
         """
         Initialize Hbase client  and create table if not already exist
         :param schema:
@@ -51,6 +50,7 @@ class HbaseCrudRepository:
 
         return entities
 
+    @retry_connection_on_ttransportexception(5)
     def find_by_id(self, id: str) -> Optional[HbaseEntity]:
         """
         Retrieves an entity by its id.
@@ -65,22 +65,30 @@ class HbaseCrudRepository:
 
         if result:
             entity = HbaseEntity()
-            entity.load(result)
+            entity.load()
 
         return entity
 
+    @retry_connection_on_brokenpipe(5)
     def find_all_prefix_id(self, prefix_id: str) -> List[HbaseEntity]:
         """
         Returns all instances of the type HbaseEntity
         with the ids that match with given prefix ID.
         :return: all entities
         """
+        entities = []
         with self.pool.connection() as con:
             table = con.table(self.table_name)
-            candlesticks = [data for key, data in table.scan(row_prefix=prefix_id)]
+            results = table.scan(row_prefix=prefix_id)
 
+            for data in results:
+                entity = HbaseEntity()
+                entity.load()
+                entities.append(entity)
 
-    @abstractmethod
+        return entities
+
+    @retry_connection_on_brokenpipe(5)
     def find_all_by_id(self, ids: List[str]) -> List[HbaseEntity]:
         """
         Returns all instances of the type HbaseEntity with the given IDs.
@@ -89,36 +97,76 @@ class HbaseCrudRepository:
         :param ids: must not be null nor contain any null values.
         :return: guaranteed to be not null. The size can be equal or less than the number of given ids.
         """
-        pass
+        entities = []
+        with self.pool.connection() as con:
+            table = con.table(self.table_name)
+            results = table.rows(rows=ids)
 
-    @abstractmethod
+            for data in results:
+                entity = HbaseEntity()
+                entity.load()
+                entities.append(entity)
+
+        return entities
+
+    @retry_connection_on_brokenpipe(5)
+    def find_all_between_ids(self, start_id: str, end_id: str) -> List[HbaseEntity]:
+        """
+        Returns all instances of the type HbaseEntity between two IDs.
+        If some or all ids are not found, no entities are returned for these IDs.
+        Note that the order of elements in the result is not guaranteed.
+        :param start_id: ID where the scanner should start.
+        :param end_id: ID where the scanner should stop.
+        :return: guaranteed to be not null.
+        """
+        entities = []
+        with self.pool.connection() as con:
+            table = con.table(self.table_name)
+            results = table.scan(row_start=start_id, row_stop=end_id)
+
+            for data in results:
+                entity = HbaseEntity()
+                entity.load()
+                entities.append(entity)
+
+        return entities
+
+    @retry_connection_on_ttransportexception(5)
     def exists_by_id(self, id: str) -> bool:
         """
         Returns whether an entity with the given id exists.
         :param id: must not be null.
         :return: true if an entity with the given id exists, false otherwise.
         """
-        pass
+        return self.find_all_by_id() is not None
 
-    @abstractmethod
-    def count(self) -> int:
+    @retry_connection_on_brokenpipe(5)
+    def count(self, filter: str=None) -> int:
         """
         Returns the number of entities available.
-        :param id:
+        :param filter: a filter string (optional)
         :return: the number of entities.
         """
-        pass
+        count = 0
+        with self.pool.connection() as con:
+            table = con.table(self.table_name)
+            for _ in table.scan(filter=filter):
+                count += 1
 
-    @abstractmethod
-    def delete(self, data: HbaseEntity) -> None:
+        return count
+
+    @retry_connection_on_ttransportexception(5)
+    def delete(self, entity: HbaseEntity) -> None:
         """
         Deletes a given entity.
-        :param data: must not be null.
+        :param entity: must not be null.
         :return:
         """
-        pass
+        with self.pool.connection() as con:
+            table = con.table(self.table_name)
+            table.delete(row=entity.id())
 
-    @abstractmethod
+    @retry_connection_on_ttransportexception(5)
     def delete_by_id(self, id: str) -> None:
         """
         Deletes the entity with the given id.
@@ -126,31 +174,58 @@ class HbaseCrudRepository:
         :param id: must not be null.
         :return:
         """
-        pass
+        with self.pool.connection() as con:
+            table = con.table(self.table_name)
+            table.delete(row=id)
 
-    @abstractmethod
+    @retry_connection_on_brokenpipe(5)
     def delete_all(self) -> None:
         """
         Deletes all entities managed by the repository.
         :return:
         """
-        pass
+        with self.pool.connection() as con:
+            if not con.is_table_enabled(self.table_name):
+                con.disable_table(self.table_name)
+            con.delete_table(self.table_name)
 
-    @abstractmethod
-    def delete_all_by_entities(self, entities: List[HbaseEntity]) -> None:
+    @retry_connection_on_brokenpipe(5)
+    def delete_all_by_entities(self, entities: List[HbaseEntity], **options) -> None:
         """
         Deletes the given entities.
         :param entities: must not be null. Must not contain null elements.
         :return:
         """
-        pass
+        with self.pool.connection() as con:
+            table = con.table(self.table_name)
+            with table.batch(**options) as b:
+                for entity in entities:
+                    b.delete(entity.id())
 
-    @abstractmethod
-    def delete_all_by_id(self, ids: List[str]) -> None:
+    @retry_connection_on_brokenpipe(5)
+    def delete_all_by_id(self, ids: List[str], **options) -> None:
         """
         Deletes all instances of the type HbaseEntity with the given IDs.
         Entities that aren't found in the persistence store are silently ignored.
         :param ids: must not be null. Must not contain null elements.
         :return:
         """
-        pass
+        with self.pool.connection() as con:
+            table = con.table(self.table_name)
+            with table.batch(**options) as b:
+                for id in ids:
+                    b.delete(id)
+
+    def __create_if_not_exist_table(self, schema: Dict[str,Dict]) -> None:
+        """
+        Create table with Hbase client  if not exist.
+        :param schema: table  schema.
+        :return:
+        """
+        try:
+            with self.pool.connection() as con:
+                list_tables = con.tables()
+                if self.table_name.encode("utf-8") not in list_tables:
+                    con.create_table(self.table_name, schema)
+        except IOError:
+            print("WARN tried to create table BINANCE whereas already created...")
