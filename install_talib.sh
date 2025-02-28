4908b14304524a2f190a26109a1a95a08881690e
kevL974 <kev.laoussing@gmail.com>
2025-02-28 01:11:59 +0100

    update install talib script

--- install_talib.sh
+++ install_talib.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz \
-&& sudo tar -xzf ta-lib-0.4.0-src.tar.gz \
-&& sudo rm ta-lib-0.4.0-src.tar.gz \
-&& cd ta-lib/ \
+wget https://github.com/ta-lib/ta-lib/releases/download/v0.6.4/ta-lib-0.6.4-src.tar.gz \
+&& sudo tar -xzf ta-lib-0.6.4-src.tar.gz \
+&& sudo rm ta-lib-0.6.4-src.tar.gz \
+&& cd ta-lib-0.6.4/ \
 && sudo ./configure --prefix=/usr \
 && sudo make \
 && sudo make install \
-&& cd ~ \
-&& sudo rm -rf ta-lib/ \
-&& pip3 install ta-lib
+&& cd .. \
+&& sudo rm -rf ta-lib-0.6.4/ \
+&& pip install ta-lib --break-system-packages
