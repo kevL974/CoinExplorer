6612426d778f9dd14d9cbe428efc68501a41bea8
kevL974 <kev.laoussing@gmail.com>
2025-08-20 21:34:19 +0200

    implementation of RestestSmaStep class in step.py

--- opa/trading/step.py
+++ opa/trading/step.py
@@ -1,4 +1,7 @@
 from __future__ import annotations
+
+from os import MFD_ALLOW_SEALING
+
 from opa.trading.context import TradingContext
 from opa.trading.technic.analysis import *
 
@@ -59,16 +62,16 @@ class CheckBullRunStep(TradingStep):
         self._id_rsi: str = id_rsi
 
     def check_condition(self) -> None:
-        sma_short = self.context.indicator_history(self._id_sma_short)
-        sma_long = self.context.indicator_history(self._id_sma_long)
-        rsi = self.context.indicator_history(self._id_rsi)
+        sma_short = self.context.indicator_value(self._id_sma_short)
+        sma_long = self.context.indicator_value(self._id_sma_long)
+        rsi = self.context.indicator_value(self._id_rsi)
 
         if(sma_short.ndim > 0) and (len(sma_short) > 0):
-            if (sma_short[-1] > sma_long[-1]) and (rsi[-1] > 50.0):
-                print(f"Check bull run : sma_short ({sma_short[-1]}) > sma_long ({sma_long[-1]} and rsi ({rsi[-1]})")
+            if (sma_short > sma_long) and (rsi > 50.0):
+                print(f"Check bull run : sma_short ({sma_short}) > sma_long ({sma_long} and rsi ({rsi})")
                 self.on_success()
             else:
-                print(f"Check bull run : sma_short ({sma_short[-1]}) < sma_long ({sma_long[-1]} and rsi ({rsi[-1]})")
+                print(f"Check bull run : sma_short ({sma_short}) < sma_long ({sma_long} and rsi ({rsi})")
                 self.on_fail()
 
     def on_fail(self) -> None:
@@ -80,18 +83,96 @@ class CheckBullRunStep(TradingStep):
 
 class RetestSmaStep(TradingStep):
 
+    INIT: int = 0
+    START: int = 1
+    SMA_ABOVE_CONVERGENT: int = 11
+    SMA_ABOVE_WAITING_DIVERGENCE: int = 12
+    SMA_ABOVE_DIVERGENT: int = 13
+    SMA_BELOW_CONVERGENT: int = 21
+    SMA_BELOW_WAITING_DIVERGENCE: int = 22
+    SMA_BELOW_DIVERGENT: int = 23
+    FAIL: int = 30
+    SUCCESS: int = 40
+
     def __init__(self, id_sma):
         super().__init__()
         self._id_sma: str = id_sma
+        self._n: int = 0
+        self._d: int = 0
+        self._state: int = self.INIT
+        self._last_price: float = 0
+        self._last_sma: float = 0
 
     def check_condition(self) -> None:
-        sma = self.context.indicator_history(self._id_sma)
-        current = self.context._environment
-        if(sma.ndim > 0) and (len(sma) > 0):
+        try :
+            price = self.context.indicator_value()
+            sma = self.context.indicator_value(self._id_sma)
+        except ValueError:
+            print(f"ReTestSmaStep - No price or sma value available")
+            self.on_wait()
 
+        if self._state == self.INIT:
+            self._state = self.START
+
+        elif self._state == self.START:
+            if self.sma_converes_to_price(sma, price) and self.is_sma_above(sma, price):
+                self._state = self.SMA_ABOVE_CONVERGENT
+            elif self.sma_converes_to_price(sma, price) and not self.is_sma_above(sma, price):
+                self._state = self.SMA_BELOW_CONVERGENT
+            else:
+                self._state = self.FAIL
+
+        elif self._state == self.SMA_ABOVE_CONVERGENT:
+            if not self.is_sma_above(sma, price):
+                self._state = self.SMA_ABOVE_WAITING_DIVERGENCE
+                self._d+=1
+
+        elif self._state == self.SMA_BELOW_WAITING_DIVERGENCE:
+            if self.is_sma_above(sma, price) and self._d < 5:
+                self._state = self.SUCCESS
+            elif not self.is_sma_above(sma, price) and self._d < 5:
+                self._d+=1
+            else:
+                self._state = self.FAIL
+
+        elif self._state == self.SMA_BELOW_CONVERGENT:
+            if self.is_sma_above(sma, price):
+                self._state = self.SMA_BELOW_WAITING_DIVERGENCE
+
+        elif self._state == self.SMA_BELOW_WAITING_DIVERGENCE:
+            if not self.is_sma_above(sma, price)  and self._d < 5:
+                self._state = self.SUCCESS
+            elif self.is_sma_above(sma, price) and self._d < 5:
+                self._d+=1
+            else:
+                self._state = self.FAIL
+
+        self._last_sma = sma
+        self._last_price = price
+
+        if self._state == self.FAIL:
+            self.on_fail()
+        elif self._state == self.SUCCESS:
+            self.on_success()
+        else:
+            self.on_wait()
+
+    def is_sma_above(self, sma: float, price: float) -> bool:
+        return sma > price
+
+    def diff(self, sma: float, price: float) -> float:
+        if self.is_sma_above(sma, price):
+            return sma - price
+        return price - sma
+
+    def sma_converes_to_price(self, current_sma: float, current_price: float) -> bool:
+        return (self.diff(self._last_sma, self._last_price) - self.diff(current_sma, current_price)) > 0
+
+    def on_wait(self) -> None:
+        self.context.transition_to(self)
 
     def on_success(self) -> None:
-        pass
+        self.context.transition_to(self.next)
 
     def on_fail(self) -> None:
-        pass
+        self.context.first_step()
