b3ff3265dd2b537a246e0ad3913851bd0fd26aa2
kevL974 <kev.laoussing@gmail.com>
2026-04-26 19:42:57 +0200

    fix : circular import inside src/opa/trading/steps/base.py

--- src/opa/trading/steps/base.py
+++ src/opa/trading/steps/base.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING
 
-from opa.trading.strategy import TradingStrategy
+if TYPE_CHECKING:
+    from opa.trading.strategy import TradingStrategy
 
 import logging
 
